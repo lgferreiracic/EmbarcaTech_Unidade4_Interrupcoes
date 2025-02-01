330ac8136f96ede6e1261eff69e2eb608a794972
lgferreiracic <lgferreira.cic@uesc.br>
2025-01-31 23:27:06 -0300

    Correção no arquivo pio e led piscando

--- EmbarcaTech_Unidade4_Interrupcoes.c
+++ EmbarcaTech_Unidade4_Interrupcoes.c
@@ -3,7 +3,7 @@
 #include "hardware/timer.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "WS2812.pio.h"
+#include "ws2812.pio.h"
 
 #define NUM_PIXELS 25
 #define TIME 200
@@ -16,14 +16,16 @@ const uint WS2812_PIN = 7; // WS2812 => GPIO7
 const uint button_a = 5; // Botão A = 5
 const uint button_b = 6; // Botão B = 6 
 const uint luminosity_R = 0; // Luminosidade máxima do LED vermelho
-const uint luminosity_G = 200; // Luminosidade máxima do LED verde
-const uint luminosity_B = 0; // Luminosidade máxima do LED azul
+const uint luminosity_G = 0; // Luminosidade máxima do LED verde
+const uint luminosity_B = 200; // Luminosidade máxima do LED azul
+
+int number_ws2812 = 0; // Variável para armazenar o número a ser exibido
 
 // Buffers para armazenar quais LEDs estão ligados matriz 5x5
 bool number_0[NUM_PIXELS] = {
     0, 1, 1, 1, 0,
     0, 1, 0, 1, 0,
-    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
     0, 1, 0, 1, 0,
     0, 1, 1, 1, 0
 };
@@ -100,6 +102,17 @@ bool number_9[NUM_PIXELS] = {
     0, 1, 1, 1, 0
 };
 
+// Função para converter a posição do matriz para uma posição do vetor.
+int getIndex(int x, int y) {
+    // Se a linha for par (0, 2, 4), percorremos da esquerda para a direita.
+    // Se a linha for ímpar (1, 3), percorremos da direita para a esquerda.
+    if (y % 2 == 0) {
+        return 24-(y * 5 + x); // Linha par (esquerda para direita).
+    } else {
+        return 24-(y * 5 + (4 - x)); // Linha ímpar (direita para esquerda).
+    }
+}
+
 static inline void put_pixel(uint32_t pixel_grb)
 {
     pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
@@ -115,11 +128,15 @@ void set_ledS(bool number[NUM_PIXELS], uint8_t r, uint8_t g, uint8_t b)
 {
     // Define a cor com base nos parâmetros fornecidos
     uint32_t color = urgb_u32(luminosity_R, luminosity_G, luminosity_B);
-
+    
     // Define todos os LEDs com a cor especificada
     for (int i = 0; i < NUM_PIXELS; i++)
-    {
-        if (number[i])
+    {   
+        int x = i % 5;
+        int y = i / 5;
+        int index = getIndex(x, y);
+
+        if (number[index])
         {
             put_pixel(color); // Liga o LED com um no buffer
         }
@@ -151,11 +168,19 @@ int main()
     gpio_set_dir(button_b, GPIO_IN);
     gpio_pull_up(button_b);
 
+    //Configurações da PIO
+    PIO pio = pio0; 
+    uint offset = pio_add_program(pio, &pio_matrix_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    pio_matrix_program_init(pio, sm, offset, WS2812_PIN);
 
     // Loop principal
     while (true)
     {
-        
-
+        // LED vermelho pisca 5 vezes por segundo
+        gpio_put(led_R, 1);
+        sleep_ms(TIME);
+        gpio_put(led_R, 0);
+        sleep_ms(TIME);
     }
 }
