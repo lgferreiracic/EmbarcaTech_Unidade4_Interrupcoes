920d4a1270a3fa26ccb820dbff7774c5e6908cbe
lgferreiracic <lgferreira.cic@uesc.br>
2025-01-31 22:55:39 -0300

    Funções e buffers para os numeros na WS2812

--- EmbarcaTech_Unidade4_Interrupcoes.c
+++ EmbarcaTech_Unidade4_Interrupcoes.c
@@ -19,6 +19,117 @@ const uint luminosity_R = 0; // Luminosidade máxima do LED vermelho
 const uint luminosity_G = 200; // Luminosidade máxima do LED verde
 const uint luminosity_B = 0; // Luminosidade máxima do LED azul
 
+// Buffers para armazenar quais LEDs estão ligados matriz 5x5
+bool number_0[NUM_PIXELS] = {
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0
+};
+
+bool number_1[NUM_PIXELS] = {
+    0, 0, 0, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 0, 0, 1, 0
+};
+
+bool number_2[NUM_PIXELS] = {
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 0, 0, 0,
+    0, 1, 1, 1, 0
+};
+
+bool number_3[NUM_PIXELS] = {
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0
+};
+
+bool number_4[NUM_PIXELS] = {
+    0, 1, 0, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 0, 0, 1, 0
+};
+
+bool number_5[NUM_PIXELS] = {
+    0, 1, 1, 1, 0,
+    0, 1, 0, 0, 0,
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0
+};
+
+bool number_6[NUM_PIXELS] = {
+    0, 1, 1, 1, 0,
+    0, 1, 0, 0, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0
+};
+
+bool number_7[NUM_PIXELS] = {
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 0, 0, 1, 0
+};
+
+bool number_8[NUM_PIXELS] = {
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0
+};
+
+bool number_9[NUM_PIXELS] = {
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0
+};
+
+static inline void put_pixel(uint32_t pixel_grb)
+{
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
+}
+
+
+void set_ledS(bool number[NUM_PIXELS], uint8_t r, uint8_t g, uint8_t b)
+{
+    // Define a cor com base nos parâmetros fornecidos
+    uint32_t color = urgb_u32(luminosity_R, luminosity_G, luminosity_B);
+
+    // Define todos os LEDs com a cor especificada
+    for (int i = 0; i < NUM_PIXELS; i++)
+    {
+        if (number[i])
+        {
+            put_pixel(color); // Liga o LED com um no buffer
+        }
+        else
+        {
+            put_pixel(0);  // Desliga os LEDs com zero no buffer
+        }
+    }
+}
+
 int main()
 {
     // Inicializações
